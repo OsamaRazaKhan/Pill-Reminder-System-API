11c579cf9c5303258263005a5025c1026aa51375
OsamaRazaKhan <osamaraza691@gmail.com>
2024-08-25 11:16:35 +0500

    app start folder added

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Pill_Reminder_System_api24
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
